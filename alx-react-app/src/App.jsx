a711f1e16f66c05fab7e475de40cbe5cfd405efb
Mustapha Abdulai <mazbandago@gmail.com>
2025-02-16 20:44:45 +0000

    add 3 components at a go

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,14 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
 
 function App() {
   return (
      <>
       <WelcomeMessage/>
+      <Header/>
+      <MainContent/>
+      <Footer/>
      </>
-    >
-     
-  )
+  );
 }
 export default App
