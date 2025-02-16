d5d8941007560a1d08beef3e5813cc9b26adaa00
Mustapha Abdulai <mazbandago@gmail.com>
2025-02-16 07:14:37 +0000

    add components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './WelcomeMessage' 
+import WelcomeMessage from './assets/WelcomeMessage' 
 
 function App() {
   return (
     <><div>
       < WelcomeMessage/>
     </div>
-     
     </>
   );
 }
