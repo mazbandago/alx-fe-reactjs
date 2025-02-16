8c0946ff16a1be9cc45032d54e61455c1c2e07cf
Mustapha Abdulai <mazbandago@gmail.com>
2025-02-16 21:53:55 +0000

    rearrange

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -12,9 +12,9 @@ function App() {
   return (
      <>
       <WelcomeMessage/>
-      "<Header/>",
-      "<Footer/>", 
-      "<MainContent/>"
+      <Header/>
+      <MainContent/>
+      <Footer/> 
       <UserProfile name='Alice' age='25' bio='Loves hiking and photography'/>
      </>
   )
