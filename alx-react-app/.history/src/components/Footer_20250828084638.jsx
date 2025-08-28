c82d85039bfe0246863e5c58666ee0e12abbae52
Mustapha Abdulai <mazbandago@gmail.com>
2025-08-28 15:55:04 +0000

    feature: header, footer and main section done

---
+++ alx-react-app/.history/src/components/Footer_20250828084638.jsx
@@ -0,0 +1,8 @@
+function Footer (){
+    return(
+        <footer>
+            <p>"export", "footer", "Footer", "2023 City Lovers"</p>
+        </footer>
+    );
+}
+export default Footer; 
--- alx-react-app/.history/src/components/Footer_20250828155011.jsx
+++ alx-react-app/.history/src/components/Footer_20250828155011.jsx
@@ -1,7 +1,7 @@
 function Footer (){
     return(
         <footer>
-            <p>&copy 2023 City Lovers</p>
+            <p> && 2023 City Lovers</p>
         </footer> 
     );
 }
