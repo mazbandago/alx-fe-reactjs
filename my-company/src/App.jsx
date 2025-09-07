03f25a9598193243dfe091d84fc5ca0be63c6f9f
Mustapha Abdulai <mazbandago@gmail.com>
2025-09-07 22:44:18 +0000

    feature: update App and contact

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
-import Footer from './components/Footer'; // optional
-
+import Navbar from './components/Navbar';
 function App() {
   return (
     <BrowserRouter>
