eaccd6447b1fe32183256aa8187e8450be490b69
Mustapha Abdulai <mazbandago@gmail.com>
2025-09-07 08:59:37 +0000

    add app.jsx file

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -6,11 +6,13 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter';
 
 function App() {
   return (
      <>
       <WelcomeMessage />
+      <Counter />
       <Header />
       <MainContent />
       <Footer />
