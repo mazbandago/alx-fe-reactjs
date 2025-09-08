482f45e683cc3273c5fd439e3c681e9ae1740e1f
Mustapha Abdulai <mazbandago@gmail.com>
2025-09-08 07:46:41 +0000

    add app

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -4,7 +4,7 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
-import Contact from './components/Contact';
+import Contact from '../src/components/Contact';
 
 function App() {
   return (
