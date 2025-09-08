759b92d1e7bbb5a338ea24e7e55bf3458de947c3
Mustapha Abdulai <mazbandago@gmail.com>
2025-09-08 06:46:59 +0000

    feature: add footer, navbar

---
+++ my-company/.history/src/components/Footer_20250908062644.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <footer style={{
+      backgroundColor: '#f1f1f1',
+      textAlign: 'center',
+      padding: '10px',
+      marginTop: '20px'
+    }}>
+      <p>&copy; 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
