f07b867bca8518bd0353aba227097572aad14ee4
Mustapha Abdulai <mazbandago@gmail.com>
2025-09-13 13:14:24 +0000

    feature: AddRecipeform and recipeList added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddRecipeForm from './components/AddRecipeForm'
+import RecipeList from './components/RecipeList'
+
+export default function App() {
+  return (
+    <>
+      <AddRecipeForm />
+      <RecipeList />
+      
+    </>
+  )
+}
