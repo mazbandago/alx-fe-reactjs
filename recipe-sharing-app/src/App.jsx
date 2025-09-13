637e4b78e7a724ed5d50a33495957990470daa1c
Mustapha Abdulai <mazbandago@gmail.com>
2025-09-13 16:14:48 +0000

    feature: three components added

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
 
 function App() {
   return (
     <div>
-      
-      This is the new project
+      <AddRecipeForm />
+      <RecipeList />
 
     </div>
   )
