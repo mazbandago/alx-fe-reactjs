0146a7b94af08a97872b13abef938e31751e5e6d
Mustapha Abdulai <mazbandago@gmail.com>
2025-09-14 11:57:35 +0000

    add router to app.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
-import { Route, Routes} from 'react-router-dom';
+import { Route, Routes, Router} from 'react-router-dom';
 import RecipeDetailsWrapper from './components/RecipeDetailsWrapper';
 
 function App() {
