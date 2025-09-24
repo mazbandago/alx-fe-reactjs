80cca9f86f93a582b23cbbb82dfbbb1f43641615
Mustapha Abdulai <mazbandago@gmail.com>
2025-09-24 13:57:06 +0000

    add extra input field

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -5,19 +5,25 @@ import { useQuery } from "@tanstack/react-query";
 
 function Search() {
   const [username, setUsername] = useState("");
-  const [submittedUsername, setSubmittedUsername] = useState("");
+  const [location, setLocation] = useState("");
+  const [minRepos, setMinRepos] = useState("");
+  const [searchParams, setSearchParams] = useState(null);
 
- const { data, isLoading, isError } = useQuery({
-    queryKey: ["githubUser", submittedUsername],
-    queryFn: () => fetchUserData(submittedUsername),
-    enabled: !!submittedUsername, // Only run when there's input
+ const { data:users, isLoading, isError } = useQuery({
+    queryKey: ["githubUser", searchParams],
+    queryFn: () => fetchUserData(searchParams),
+    enabled: !!searchParams, // Only run when there's input
     staleTime: 1000 * 60 * 5, // 5 minutes
   });
  
   
    const handleSubmit = (e) => {
     e.preventDefault();
-    setSubmittedUsername(username.trim());
+    setSearchParams({
+      username:username.trim(),
+      location: location.trim(),
+      minRepos: minRepos.trim(),
+    });
   };
   
   return (
@@ -26,18 +32,40 @@ function Search() {
         <form className='cont' onSubmit ={handleSubmit}>
           <label htmlFor="usernames">Github Username</label>
           <input type="text" id='usernames' value={username} placeholder='Enter your Github username Here' onChange={(e) => setUsername(e.target.value)} />
+        <label htmlFor="location">Location</label>
+        <input
+          type="text"
+          id="location"
+          value={location}
+          placeholder="e.g. Ghana"
+          onChange={(e) => setLocation(e.target.value)}
+          // className="w-full p-2 border rounded"
+        />
+        <label htmlFor="minRepos">Minimum Repositories</label>
+        <input
+          type="number"
+          id="minRepos"
+          value={minRepos}
+          placeholder="e.g. 10"
+          onChange={(e) => setMinRepos(e.target.value)}
+          // className="w-full p-2 border rounded"
+        />
           <button type ="submit">Submit</button>
         </form>
 
-        {isLoading && <p>Loading...</p>}
+        {isLoading && <p style={{marginTop: "16px"}}>Loading...</p>}
         {isError && <p className='err'>"Looks like we can't find the user"</p>}
-        {data && (
-         <div className="user-card">
-           <img src={data.avatar_url} alt={data.login} width={100} />
-          <h2>{data.name || data.login}</h2>
-           <a href={data.html_url} target="_blank" rel="noopener noreferrer">
-             View Profile
-           </a>
+        {users && users.length > 0 && (
+         <div style={{marginTop:"24px", display:'grid', gap:"16px"}}>
+          {users.map((user) => (
+            <div key ={user.id}className='user-card'>
+              <img src={user.avatar_url} alt={user.login} style={{width:"100px", height:"100px", borderRadius:"50px"}} />
+              <h2 style={{fontSize:"18px", fontWeight:"bold"}}>{user.name || user.login}</h2>
+              <a href={user.html_url} target="_blank" rel="noopener noreferrer">
+                View Profile
+              </a>
+            </div>
+          ))}
          </div>
       )}
       </div>
--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
-export async function fetchUserData(username) {
-  const response = await axios.get(`https://api.github.com/users/${username}`);
-  return response.data;
+export async function fetchUserData({username, location, minRepos}) {
+  let query = "";
+
+  if (username) query += `${username} in:login `;
+  if (location) query += `location:${location} `;
+  if (minRepos) query += `repos:>=${minRepos}`;
+
+  const response = await axios.get(`https://api.github.com/search/users?q=${encodeURIComponent(query)}&per_page=10`);
+  return response.data.items;
 }
