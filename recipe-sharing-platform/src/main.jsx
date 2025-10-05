889377e54cb265928a55dddd1d2eb866b77c0ccb
Mustapha Abdulai <mazbandago@gmail.com>
2025-10-05 04:53:50 +0000

    initial commit

---
+++ recipe-sharing-platform/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <h1>This is the main page</h1>
+  </StrictMode>,
+)
