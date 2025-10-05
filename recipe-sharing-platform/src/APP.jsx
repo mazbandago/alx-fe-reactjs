892a19accac504b764cc0810aebf38bb518430e8
Mustapha Abdulai <mazbandago@gmail.com>
2025-10-05 20:29:23 +0000

    refactor app.jsx

--- recipe-sharing-platform/src/APP.jsx
+++ recipe-sharing-platform/src/APP.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import RecipeDetail from './components/RecipeDetail';
 
