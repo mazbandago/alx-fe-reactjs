022bde68b12bd8986d55135f78513b857d18b1d8
Mustapha Abdulai <mazbandago@gmail.com>
2025-10-05 08:34:41 +0000

    feat:created the loading page and styled it

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -10,18 +10,18 @@ function HomePage() {
         setTimeout(()=>{
             setRecipes(fetchRecipes)
             setloading(false);
-        },5000)
+        },7000)
     },[])
 
 
   return (
     <>
-    <h1>List of Recipes</h1>
-    {loading && <p>Loading...</p>}
+    <h1 className='p-4 mx-auto text-2xl font-bold sm:text-3xl text-center items-center'>List of Recipes</h1>
+    {loading && <p className='flex justify-center items-center text-5xl font-bold text-center my-5 min-h-screen rounded-xl border bg-blue-400 hover:bg-blue-600'>Loading...</p>}
     {recipes && recipes.length > 0 &&(
         recipes.map((recipe) =>(
             <div key={recipe.id}>
-                <h2>{recipe.tittle}</h2>
+                <h2>{recipe.title}</h2>
                 <p>{recipe.summary}</p>
                 <img src={recipe.image} alt={recipe.tittle} />
             </div>
