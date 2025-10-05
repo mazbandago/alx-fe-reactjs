e3d40123fb3210541c41e6f57d10aa8e5d2b3718
Mustapha Abdulai <mazbandago@gmail.com>
2025-10-05 20:32:48 +0000

    refactor to include App.jsx

--- recipe-sharing-platform/src/APP.jsx
+++ recipe-sharing-platform/src/APP.jsx
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import RecipeDetail from './components/RecipeDetail';
 
-
-
 function App() {
   return (
     <Router>
@@ -15,5 +13,4 @@ function App() {
     </Router>
   )
 }
-
 export default App;
