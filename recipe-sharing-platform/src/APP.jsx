5b446193a97da6f71c832c5af5edde3be7b58ee4
Mustapha Abdulai <mazbandago@gmail.com>
2025-10-06 17:22:32 +0000

    final commit

--- recipe-sharing-platform/src/APP.jsx
+++ recipe-sharing-platform/src/APP.jsx
@@ -1,22 +1,20 @@
+
 import React from 'react'
-import { BrowserRouter, Route, Router, Routes } from 'react-router';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import RecipeDetail from './components/RecipeDetail';
-
-
-
-
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-            <Route path = "/" element={<HomePage/>}/>
-            <Route path = "/recipe/:id" element = {<RecipeDetail/>} />
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/recipe/:id' element={<RecipeDetail/>}/>
         </Routes>
-    </Router>
+      </Router>
     </div>
   )
 }
+
 export default App;
