9e75b142f09706972c6601ae1c1237a64a5ef390
Mustapha Abdulai <mazbandago@gmail.com>
2025-10-14 15:05:46 +0000

    feat:created forms using formik and validated the form using formik validation

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import RegistrationForm from './components/RegistrationForm';
+import FormikForm from './components/FormikForm';
 
 function App() {
   return (
     <div>
-      <RegistrationForm />
+      <FormikForm />
     </div>
   )
 }
