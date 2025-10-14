a397a1bc6df271e9501c8614dfc634e2af25e630
Mustapha Abdulai <mazbandago@gmail.com>
2025-10-14 10:20:38 +0000

    refactor the useState to hold three inputs at a go

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,28 +1,33 @@
 import React, { useState } from 'react'
 
 function RegistrationForm() {
-    const[userName, setUsername] = useState('');
-    const[email, setEmail] = useState('');
-    const[password,setPassword] = useState('');
+    const[formData, setFormData] = useState({name:"", email:"",password:""});
+
+    const handleOnChange = (e)=>{
+        const{name,value} = e.target
+        setFormData(prevData =>({...prevData, [name]:value}))
+    }
+    
 
 const handleOnSubmit = (e) =>{
     e.preventDefault();
-    setEmail('')
-    setUsername("")
-    setPassword("")
+    console.log(formData)
+    setFormData('');
+    
+    
 }
 
 
   return (
     <div className='p-6 mx-auto max-w-4xl mt-12 mb-8 bg-white shadow-lg hover:shadow-lg/50'>
     <form onSubmit={handleOnSubmit} className='flex flex-col items-center justify-center'>
-        <input type="text" placeholder='Enter Your Full Name' value={userName} onChange={(e)=>setUsername(e.target.value)}
+        <input type="text" placeholder='Enter Your Full Name' name='name' value={formData.name} onChange={handleOnChange}
         className='border-1 rounded p-3 w-full focus:ring-gray-500 text-2xl text-gray-500 mb-6'
         />
-        <input type="email" placeholder='Enter Your Email Here' required value={email} onChange={(e)=>setEmail(e.target.value)} 
+        <input type="email" placeholder='Enter Your Email Here' name="email" required value={formData.email} onChange={handleOnChange} 
         className='border-1 rounded p-3 w-full focus:ring-gray-500 text-2xl text-gray-500 mb-6'
         />
-        <input type="password" required value={password} onChange={(e)=>setPassword(e.target.value)} 
+        <input type="password" required name="password" value={formData.password} onChange={handleOnChange} 
         className='border-1 rounded p-3 w-full focus:ring-gray-500 text-2xl text-gray-500 mb-6'
         />
         <button type="submit" className='border py-3 px-2 rounded font-bold text-2xl bg-amber-400 hover:bg-amber-200 transition-transform duration-300 hover:scale-120 shadow-2xl'>submit</button>
