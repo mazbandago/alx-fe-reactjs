9f143645fbb9695cc247710337f4e6bd64fd674f
Mustapha Abdulai <mazbandago@gmail.com>
2025-10-23 11:35:26 +0000

    feat:create form with three inputs and a button for submission

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Registration from './components/Registration';
+import React, { useState } from 'react'
+import React from 'react';
 
 function App() {
   return (
@@ -10,3 +10,26 @@ function App() {
 }
 
 export default App;
+export function Registration() {
+    const [formData, setFormData] = useState({ username: "", email: "", password: "" });
+
+    const handleonChange = () => {
+        const { name, value } = e.value;
+        setFormData(prevData => ({ ...prevData, [name]: value }));
+    };
+
+    const handleonSubmit = (e) => {
+        e.preventDefaut();
+        setFormData({ username: "", email: "", password: "" });
+    };
+
+    return (
+        <form>
+            <input type="text" name="username" onChange={() =} />
+            <input type="email" name="email" onChange={() =} />
+            <input type="password" name="password" onChange={() =} />
+            <button>Submit</button>
+
+        </form>
+    );
+}
--- form-handling-react/src/components/Registration.jsx
+++ form-handling-react/src/components/Registration.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Registration() {
+    const[formData, setFormData] = useState({username:"",email:"",password:""});
+
+    const handleonChange = ()=>{
+        const{name, value} = e.value;
+        setFormData(prevData =>({...prevData, [name]:value }))
+    }
+
+    const handleonSubmit = (e)=>{
+        e.preventDefaut()
+        console.log(formData)
+        setFormData({username:"",email:"", password:""})
+    }
+
   return (
-    <h1>This is the registration page</h1>
+   <form onSubmit={handleonSubmit}>
+    <input type="text" name="username" value={formData.username} onChange={handleonChange} />
+    <input type="email" name="email" value ={formData.email} onChange={handleonChange}/>
+    <input type="password"  name="password" value={formData.password} onChange={handleonChange}/>
+    <Button type="submit">Submit</Button>
+   </form>
   )
 }
 
