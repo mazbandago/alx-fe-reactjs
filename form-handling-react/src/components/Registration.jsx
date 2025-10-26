292dc1b5f7c5fb4a5dd6bf92f1483e4bece5fb74
Mustapha Abdulai <mazbandago@gmail.com>
2025-10-26 11:36:57 +0000

    feat:installed tailwind and styled both formik form and registration form

--- form-handling-react/src/components/Registration.jsx
+++ form-handling-react/src/components/Registration.jsx
@@ -33,23 +33,23 @@ export default function RegistrationForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Username:</label>
-        <input name="username" value={formData.username} onChange={handleChange} />
+    <form onSubmit={handleSubmit} className='p-6 mx-auto max-w-4xl bg-white rounded-2xl border-0 shadow-xl hover:shadow-lg/200 mb-10'>
+      <div className=''>
+        <label  className='font-bold'>Username:</label>
+        <input name="username" value={formData.username} onChange={handleChange} className='p-3 border-1 mb-4 text-2xl text-gray-600 rounded border-gray-700 mt-8 w-full'/>
         {errors.username && <span>{errors.username}</span>}
       </div>
       <div>
-        <label>Email:</label>
-        <input name="email" value={formData.email} onChange={handleChange} />
+        <label className='font-bold '>Email:</label>
+        <input name="email" value={formData.email} onChange={handleChange} className='p-3 border-1 mb-4 text-2xl text-gray-600 rounded border-gray-700 mt-8 w-full'/>
         {errors.email && <span>{errors.email}</span>}
       </div>
       <div>
-        <label>Password:</label>
-        <input name="password" type="password" value={formData.password} onChange={handleChange} />
+        <label className='font-bold'>Password:</label>
+        <input name="password" type="password" value={formData.password} onChange={handleChange}className='p-3 border-1 mb-4 text-2xl text-gray-600 rounded border-gray-700 mt-8 w-full'/>
         {errors.password && <span>{errors.password}</span>}
       </div>
-      <button type="submit">Register</button>
-    </form>
+      <button type="submit" className='border p-2 rounded bg-amber-400 mx-auto hover:bg-amber-700'>Register</button>
+    </form> 
   );
 }
--- form-handling-react/src/components/formikForm.jsx
+++ form-handling-react/src/components/formikForm.jsx
@@ -18,21 +18,20 @@ export default function FormikForm() {
 
   return (
     <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
-      <Form>
+      <Form  className='p-6 mx-auto max-w-4xl bg-white rounded-2xl border-0 shadow-xl hover:shadow-lg/200 mb-10'>
         <div>
-          <label>Username:</label>
-          <Field name="username" />
+          <label className='font-bold'>Username:</label>
+          <Field name="username"className='p-3 border-1 mb-4 text-2xl text-gray-600 rounded border-gray-700 mt-8 w-full' />
           <ErrorMessage name="username" component="span" />
         </div>
         <div>
-          <label>Email:</label>
-          <Field name="email" type="email" />
+          <label className='font-bold'>Email:</label>
+          <Field name="email" type="email" className='p-3 border-1 mb-4 text-2xl text-gray-600 rounded border-gray-700 mt-8 w-full'/>
           <ErrorMessage name="email" component="span" />
         </div>
         <div>
-          <label>Password:</label>
-          <Field name="password" type="password" />
-          <ErrorMessage name="password" component="span" />
+          <label className='font-bold'>Password:</label>
+          <Field name="password" type="password" className='p-3 border-1 mb-4 text-2xl text-gray-600 rounded border-gray-700 mt-8 w-full' />
         </div>
         <button type="submit">Register</button>
       </Form>
